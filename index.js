8c19a444165c91bed7aec90049a951c3c1a1b928
Student <cjethva149>
2022-02-01 09:43:21 +0530

    bug

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
  var http = require('http');
  var express = require('express');
- var bodyParser = require('body-parser');
- var cors = require('cors');
- var appRoute = require('./Routes/appRoutes');
 
 
  app = express();
- app.use(cors());
- app.use(bodyParser.urlencoded({
-     extended: true
- }));
- app.use(bodyParser.json());
- app.use('/', appRoute);
 
-
+ app.get('/',(req,res)=>{
+     res.sendfile('index.html',{root:__dirname})
+ })
 
  http.createServer(app).listen(process.env.PORT||3000);
  console.log("server started");
